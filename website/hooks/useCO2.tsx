13c1f365af0a692566a6cb3d77b0fd59a9bfa0bf
Parik Vahlen <patrik.vahlen@seb.se>
2023-03-14 14:37:12 +0100

    added socket io

--- website/hooks/useCO2.tsx
+++ website/hooks/useCO2.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { averageIntensity } from "@tgwf/co2";
 import { sustainableWebDesign } from "@/utility/co2";
+import {io} from "socket.io-client";
 
 export interface IReport {
   url: string;
@@ -9,39 +10,52 @@ export interface IReport {
   bytes: number;
 }
 
+// const socket = io("http://localhost:3001/report");
+
 export default function useCO2(url: string) {
   const [state, setState] = useState<IReport>();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    if (!url) return;
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const headers  = {"Content-Type" : "application/json"}
-        const response = await fetch("http://localhost:3001/report", {headers, method: "POST", body: JSON.stringify({url})});
-        const json = await response.json();
-        console.log(json);
+    useEffect(() => {
+      if(!url){
+        return
+      }
 
-        const bytes = json.sumSize;
+      const socket = io("http://localhost:3001");
+      socket.emit("payload", url);
 
+      function onConnect() {
+        // setIsConnected(true);
+      }
+  
+      function onDisconnect() {
+        // setIsConnected(false);
+      }
+  
+      function onResultEvent(value : any) {
+        const bytes = value.sumSize;
+        console.log(bytes);
         setState({
           url: url,
           co2: sustainableWebDesign.perByte(bytes),
           co2_intensity: parseFloat(averageIntensity.data.SWE),
           bytes: bytes,
         });
-      } catch (error) {
-        setError(`Failed to generate a report for ${url}`);
-        console.log("error", error);
-      } finally {
-        setIsLoading(false);
       }
-    };
+  
+      socket.on('connect', onConnect);
+      socket.on('disconnect', onDisconnect);
+      socket.on('result', onResultEvent);
+  
+      return () => {
+        console.log('CLOSED');
+        socket.off('connect', onConnect);
+        socket.off('disconnect', onDisconnect);
+        socket.off('result', onResultEvent);
+      };
+    }, [url]);
 
-    fetchData();
-  }, [url]);
   return {
     state,
     isLoading,
