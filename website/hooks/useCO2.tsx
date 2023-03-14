09e3a90d48ec541c10d3d04e95f6bf94756ffa81
Rågmo <maximilian.ragmo@seb.se>
2023-03-14 14:57:09 +0100

    progressbar

--- website/hooks/useCO2.tsx
+++ website/hooks/useCO2.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { averageIntensity } from "@tgwf/co2";
 import { sustainableWebDesign } from "@/utility/co2";
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
 export interface IReport {
   url: string;
@@ -10,60 +10,59 @@ export interface IReport {
   bytes: number;
 }
 
-// const socket = io("http://localhost:3001/report");
-
 export default function useCO2(url: string) {
   const [state, setState] = useState<IReport>();
   const [status, setStatus] = useState(0);
+  const [isGenerating, setIsGenerating] = useState(false);
   const [error, setError] = useState("");
 
-    useEffect(() => {
-      if(!url){
-        return
-      }
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
 
-      const socket = io("http://localhost:3001");
-      socket.emit("payload", url);
+    const socket = io("http://localhost:3001");
+    socket.emit("payload", url);
 
-      function onConnect() {
-        // setIsConnected(true);
-      }
-  
-      function onDisconnect() {
-        // setIsConnected(false);
-      }
-  
-      function onResultEvent(value : any) {
-        const bytes = value.sumSize;
-        console.log(bytes);
-        setState({
-          url: url,
-          co2: sustainableWebDesign.perByte(bytes),
-          co2_intensity: parseFloat(averageIntensity.data.SWE),
-          bytes: bytes,
-        });
-      }
+    function onConnect() {
+      setIsGenerating(true);
+    }
 
-      function onStatusEvent(value : any) {
-       setStatus(value);
-      }
-      
-      socket.on('status', onStatusEvent);
-      socket.on('connect', onConnect);
-      socket.on('disconnect', onDisconnect);
-      socket.on('result', onResultEvent);
-  
-      return () => {
-        socket.off('status', onStatusEvent);
-        socket.off('connect', onConnect);
-        socket.off('disconnect', onDisconnect);
-        socket.off('result', onResultEvent);
-      };
-    }, [url]);
+    function onDisconnect() {
+      setIsGenerating(false);
+    }
+
+    function onResultEvent(value: any) {
+      const bytes = value.sumSize;
+      setState({
+        url: url,
+        co2: sustainableWebDesign.perByte(bytes),
+        co2_intensity: parseFloat(averageIntensity.data.SWE),
+        bytes: bytes,
+      });
+    }
+
+    function onStatusEvent(value: any) {
+      setStatus(value);
+    }
+
+    socket.on("status", onStatusEvent);
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("result", onResultEvent);
+
+    return () => {
+      socket.off("status", onStatusEvent);
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("result", onResultEvent);
+    };
+  }, [url]);
 
   return {
     state,
     status,
+    isGenerating,
     error,
   };
 }
