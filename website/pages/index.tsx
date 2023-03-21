aaaa6322037cb92efa9452971b34b59769d0d9ef
PatrikVahlen <91195238+PatrikVahlen@users.noreply.github.com>
2023-03-21 18:10:23 +0100

    fix

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -61,7 +61,7 @@ function Heading({ url }: { url?: string }) {
     </h1>
   );
 }
-//FUNCTION
+//HERE
 function Report({ report }: { report: IReport }) {
   const yearlyEmission = co2.perYear({
     bytes: report.bytes,
